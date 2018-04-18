0d7b20d882ddcdb4f44edc5cdcf7a8c5ad0ff0b4
Joseph Callen <jcallen@redhat.com>
2018-04-18 13:23:30 -0400

    changes with ingress, organization

---
+++ kubernetes/ingress/README.md
@@ -0,0 +1,4 @@
+
+Source:
+- https://akomljen.com/kubernetes-nginx-ingress-controller/
+- https://github.com/kubernetes/ingress-nginx/tree/master/deploy
---
+++ kubernetes/ingress/ingress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/namespace.yaml \
+    | kubectl apply -f -
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/default-backend.yaml \
+    | kubectl apply -f -
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/configmap.yaml \
+    | kubectl apply -f -
+
+# Enable nginx VTS Status
+kubectl patch cm nginx-configuration --type merge -p '{"data":{"enable-vts-status":"true"}}' -n ingress-nginx
+
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml \
+    | kubectl apply -f -
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml \
+    | kubectl apply -f -
+
+curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/rbac.yaml \
+    | kubectl apply -f -
+
+
+kubectl apply -f with-rbac.yaml
+
+kubectl apply -f service-nodeport.yaml
+
